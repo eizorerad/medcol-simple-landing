38dc4cba8ba3fba5610e5a1ce9114726b9e6bfde
Leonid Klarov <eizonix@gmail.com>
2026-02-10 23:25:07 +0400

    Remove decorative images from features page hero

--- components/innerpages/features/Hero.jsx
+++ components/innerpages/features/Hero.jsx
@@ -5,40 +5,6 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div id="hero_header" className="hero-header section panel overflow-hidden">
-      <div
-        className="position-absolute top-0 start-0 end-0 min-h-screen overflow-hidden d-none lg:d-block"
-        data-anime="targets: >*; scale: [0, 1]; opacity: [0, 1]; easing: spring(1, 80, 10, 0); duration: 450; delay: anime.stagger(100, {start: 750});"
-      >
-        <div
-          className="position-absolute rotate-45"
-          style={{
-            top: "30%",
-            left: "18%",
-          }}
-        >
-          <Image
-            className="w-32px text-gray-900 dark:text-white"
-            src="/assets/images/template/medcol-hero.png"
-            width={193}
-            height={216}
-            alt="medcol-hero"
-            data-uc-svg=""
-          />
-        </div>
-        <div
-          className="position-absolute rotate-45"
-          style={{ top: "15%", right: "18%" }}
-        >
-          <Image
-            className="w-24px text-gray-900 dark:text-white"
-            src="/assets/images/template/medcol-hero.png"
-            width={69}
-            height={95}
-            alt="star-2"
-            data-uc-svg=""
-          />
-        </div>
-      </div>
       <div className="section-outer panel pt-9 lg:pt-10 pb-6 xl:pb-9">
         <div className="container max-w-lg">
           <div className="section-inner panel mt-2 sm:mt-4 lg:mt-0">
