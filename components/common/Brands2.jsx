1ebc9448db1f516b69ea5a7ceb2e8c801169206e
Leonid Klarov <eizonix@gmail.com>
2026-02-10 23:21:33 +0400

    Fix brands: centered flex layout, remove Swiper for 4 items

--- components/common/Brands2.jsx
+++ components/common/Brands2.jsx
@@ -1,43 +1,27 @@
-"use client";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { brands } from "@/data/brands";
 import Image from "next/image";
 
 export default function Brands2() {
-  const swiperOptions = {
-    slidesPerView: 2, // items: 2
-    centeredSlides: true, // center: true
-    centeredSlidesBounds: true, // center-bounds: true
-    breakpoints: {
-      // data-uc-swiper-s
-      768: {
-        slidesPerView: 4, // items: 4
-        centeredSlides: false, // center: false
-        centeredSlidesBounds: false, // center-bounds: false
-      },
-      // data-uc-swiper-m
-      1024: {
-        slidesPerView: 5, // items: 5
-        spaceBetween: 80, // gap: 80
-      },
-    },
-  };
   return (
-    <Swiper {...swiperOptions}>
-      {brands.map((brand, index) => (
-        <SwiperSlide
-          className="brand-item swiper-slide text-center"
-          key={index}
+    <div className="d-flex flex-wrap justify-center items-center gap-4 lg:gap-6 xl:gap-8">
+      {brands.map((brand) => (
+        <a
+          key={brand.id}
+          href={brand.href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="brand-item text-center"
         >
           <Image
-            className="brand-item-image h-40px image-filter"
+            className="brand-item-image image-filter"
             src={brand.src}
             width={165}
             height={48}
             alt={brand.alt}
+            style={{ objectFit: "contain", width: "auto", height: "40px" }}
           />
-        </SwiperSlide>
+        </a>
       ))}
-    </Swiper>
+    </div>
   );
 }
